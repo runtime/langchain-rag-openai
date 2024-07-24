577079732c883d03b986902f064b48893bcb6c44
runtime <erik.kroha@gmail.com>
2024-07-24 19:57:48 -0400

    query_data working with one error

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,5 +1,13 @@
 #openai
 from langchain_openai import OpenAIEmbeddings
+import openai
+#env
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+openai.api_key = os.environ['OPENAI_API_KEY']
 
 def get_embedding_function():
     embeddings = OpenAIEmbeddings()
